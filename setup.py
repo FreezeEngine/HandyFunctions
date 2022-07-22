2f128397811bbeeb3ab8371963948bc8e6279461
FreezeEngine <mr.springtrap@yandex.ru>
2022-07-23 01:14:18 +0300

    Restructure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='handy-functions',
+    version='1.0',
+    packages=['HandyFunctions'],
+    url='https://github.com/FreezeEngine/HandyFunctions',
+    license='MIT',
+    author='FreezeEngine',
+    author_email='freezeengine@gmail.com',
+    description='Contains handy functions'
+)
